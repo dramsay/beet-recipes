20aad190961970fb3d8cc901efbc255bf36141cd
Doug Ramsay <doug@intridea.com>
2009-06-07 22:19:32 -0400

    Adding first stab at clearance recipe

---
+++ rails/clearance.rb
@@ -0,0 +1,27 @@
+gem "thoughtbot-clearance", :lib => "clearance", :source  => "http://gems.github.com", :version => "0.6.6"
+
+generate("clearance")
+rake("db:migrate")
+
+if yes?("Add Cucumber features? (yes/no)")
+  generate("clearance_features")
+  RAILS_ENV = 'test'
+  gem 'webrat', :version => '= 0.4.4'
+  gem 'cucumber', :version => '= 0.3.0'
+  gem 'thoughtbot-factory_girl', :lib     => 'factory_girl', :source  => "http://gems.github.com", :version => '1.2.1'
+  run('sudo gem install nokogiri')
+end
+
+todo %Q(Define a HOST constant in your environment files. In config/environments/test.rb and config/environments/development.rb it can be:
+        \n\n
+        HOST = "localhost"
+        \n\n
+        In production.rb it must be the actual host your application is deployed to. The constant is used by mailers to generate URLs in emails.
+        \n\n
+        In config/environment.rb:
+        \n\n
+        DO_NOT_REPLY = "donotreply@example.com"
+        \n\n
+        Define root_url to something in your config/routes.rb:
+        \n\n
+        map.root :controller => 'home')
